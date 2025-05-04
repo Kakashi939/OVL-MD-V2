1f7fac097fd4f0a283a53d78f8811ae5210f87e4
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-04 17:54:47 +0000

    Update and rename Like_status.js to like_status.js

---
+++ Ovl_events/Message_upsert_events/like_status.js
@@ -0,0 +1,21 @@
+const { WA_CONF } = require('.../DataBase/wa_conf');
+
+async function like_status(ovl, ms, ms_org) {
+    const settings = await WA_CONF.findOne({ where: { id: '1' } });
+    if (settings) {
+        if (ms_org === "status@broadcast" && settings.like_status !== "non" && settings.like_status) {
+            const emoji = settings.like_status;
+            await ovl.sendMessage(ms_org, { 
+                react: { 
+                    key: ms.key, 
+                    text: emoji 
+                } 
+            }, { 
+                statusJidList: [ms.key.participant, id_Bot], 
+                broadcast: true 
+            });
+        }
+    }
+}
+
+module.exports = like_status;
