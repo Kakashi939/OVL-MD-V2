5c1993b195fc9ad4a5a382050647d951ac1850f4
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-04 20:19:10 +0000

    Create index.js

---
+++ Ovl_events/Message_upsert_events/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rankAndLevelUp: require('./rankAndLevelUp'),
+  lecture_status: require('./lecture_status'),
+  like_status: require('./like_status'),
+  presence: require('./presence'),
+  dl_status: require('./dl_status'),
+  antivv: require('./antivv'),
+  antidelete: require('./antidelete'),
+  antitag: require('./antitag'),
+  antilink: require('./antilink'),
+  antibot: require('./antibot'),
+};
