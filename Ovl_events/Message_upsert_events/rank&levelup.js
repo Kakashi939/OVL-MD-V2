0d77866d2aa0cd55516ee3f1e3c9f4886378d143
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-04 23:35:56 +0000

    Update rank&levelup.js

--- Ovl_events/Message_upsert_events/rank&levelup.js
+++ Ovl_events/Message_upsert_events/rank&levelup.js
@@ -1,5 +1,5 @@
-const { levels, calculateLevel } = require('../../../DataBase/levels');
-const { Ranks } = require('../../../DataBase/rank');
+const { levels, calculateLevel } = require('../../DataBase/levels');
+const { Ranks } = require('../../DataBase/rank');
 
 async function rankAndLevelUp(ovl, ms_org, texte, auteur_Message, nom_Auteur_Message, config) {
     if (texte && auteur_Message.endsWith("s.whatsapp.net")) {
