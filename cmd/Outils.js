b7e5d9902b44212868db10bd60e78e85db6c1f80
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-09 04:41:50 +0000

    Update Outils.js

--- cmd/Outils.js
+++ cmd/Outils.js
@@ -19,7 +19,7 @@ ovlcmd(
     },
     async (ms_org, ovl, cmd_options) => {
         try {
-             const rawUrl = 'https://cdn.jsdelivr.net/gh/Ainz-devs/OVL-THEME/themes.json';
+             const rawUrl = 'https://raw.githubusercontent.com/Ainz-devs/OVL-THEME/refs/heads/main/themes.json';
 
             const { data: themes } = await axios.get(rawUrl);
 
@@ -115,7 +115,7 @@ ovlcmd(
   async (ms_org, ovl, cmd_options) => {
     try {
       const args = cmd_options.arg;
-      const rawUrl = 'https://cdn.jsdelivr.net/gh/Ainz-devs/OVL-THEME/themes.json';
+      const rawUrl = 'https://raw.githubusercontent.com/Ainz-devs/OVL-THEME/refs/heads/main/themes.json';
 
       const { data: themesData } = await axios.get(rawUrl);
 
@@ -169,7 +169,7 @@ ovlcmd(
             if (m > 0) uptime += `${m}M `;
             if (s > 0) uptime += `${s}S`;
 
-            const rawUrl = 'https://cdn.jsdelivr.net/gh/Ainz-devs/OVL-THEME/themes.json';
+            const rawUrl = 'https://raw.githubusercontent.com/Ainz-devs/OVL-THEME/refs/heads/main/themes.json';
 
             const { data: themes } = await axios.get(rawUrl);
 
