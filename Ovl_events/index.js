1701f4d6040bbd14ca1201a2621365c4f8d6ee8b
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-09 03:48:53 +0000

    Update index.js

--- Ovl_events/index.js
+++ Ovl_events/index.js
@@ -1,7 +1,9 @@
+const { dl_save_media_ms, recup_msg } = require('./autres_fonctions');
+
 module.exports = {
   message_upsert: require('./message_upsert'),
   group_participants_update: require('./group_participants_update'),
   connection_update: require('./connection'),
-  dl_save_media_ms: require('./autres_fonctions'),
-  recup_msg: require('./autres_fonctions'),
+  dl_save_media_ms,
+  recup_msg,
 };
