9741057f7a769a4e012d457ac905e4b2177c9a39
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-17 16:44:45 +0000

    Update like_status.js

--- Ovl_events/Message_upsert_events/like_status.js
+++ Ovl_events/Message_upsert_events/like_status.js
@@ -1,6 +1,6 @@
 const { WA_CONF } = require('../../DataBase/wa_conf');
 
-async function like_status(ovl, ms, ms_org) {
+async function like_status(ovl, ms, ms_org, id_Bot) {
     const settings = await WA_CONF.findOne({ where: { id: '1' } });
     if (settings) {
         if (ms_org === "status@broadcast" && settings.like_status !== "non" && settings.like_status) {
