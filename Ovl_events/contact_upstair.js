7a034716ac1b73c52753fdcf588f5b8a0f1a503b
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-25 18:57:59 +0000

    Create contact_upstair.js

---
+++ Ovl_events/contact_upstair.js
@@ -0,0 +1,18 @@
+const contactCache = {};
+
+function updateContacts(contacts) {
+    for (const contact of contacts) {
+        contactCache[contact.id] = contact.name || contact.notify || null;
+        console.log(`🔹 Contact ajouté : ${contact.id} → ${contactCache[contact.id]}`);
+    }
+}
+
+function getUserName(jid) {
+    return contactCache[jid] || "Nom inconnu";
+}
+
+module.exports = {
+    updateContacts,
+    getUserName,
+    contactCache
+};
