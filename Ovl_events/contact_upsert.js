14c6ad249b94870d1473d036bed35e01cab1df71
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-25 19:44:26 +0000

    Update contact_upsert.js

--- Ovl_events/contact_upsert.js
+++ Ovl_events/contact_upsert.js
@@ -4,6 +4,7 @@ function updateContacts(contacts) {
     for (const contact of contacts) {
         contactCache[contact.id] = contact.name || contact.notify || null;
         console.log(`🔹 Contact ajouté : ${contact.id} → ${contactCache[contact.id]}`);
+        console.log(contacts);
     }
 }
 
