60c97ec7f5816c8744b5871a3e30857bc86534f4
Ainz-devs <debbi52249@x8to.underseagolf.com>
2025-05-26 09:54:05 +0000

    Update rank&levelup.js

--- Ovl_events/Message_upsert_events/rank&levelup.js
+++ Ovl_events/Message_upsert_events/rank&levelup.js
@@ -1,12 +1,18 @@
 const { levels, calculateLevel } = require('../../DataBase/levels');
 const { Ranks } = require('../../DataBase/rank');
+const { changerPseudo, ajouterUtilisateur, getInfosUtilisateur } = require('../../DataBase/economie');
 
 async function rankAndLevelUp(ovl, ms_org, texte, auteur_Message, nom_Auteur_Message, config) {
     if (texte && auteur_Message.endsWith("s.whatsapp.net")) {
-        let userId = auteur_Message;
-        const user = await Ranks.findOne({ where: { id: userId } });
+        const userId = auteur_Message;
+        const infosUser = await getInfosUtilisateur(userId);
+        if (!infosUser) {
+            await ajouterUtilisateur(userId, nom_Auteur_Message);
+        }
+        await changerPseudo(userId, nom_Auteur_Message);
+        let user = await Ranks.findOne({ where: { id: userId } });
         if (!user) {
-            await Ranks.create({
+            user = await Ranks.create({
                 id: userId,
                 name: nom_Auteur_Message,
                 level: 0,
@@ -17,15 +23,12 @@ async function rankAndLevelUp(ovl, ms_org, texte, auteur_Message, nom_Auteur_Mes
             user.name = nom_Auteur_Message;
             user.messages += 1;
             user.exp += 10;
-
             const newLevel = calculateLevel(user.exp);
-
-            if (newLevel > user.level && config.LEVEL_UP == 'oui') {
+            if (newLevel > user.level && config.LEVEL_UP === 'oui') {
                 await ovl.sendMessage(ms_org, {
                     text: `Félicitations ${nom_Auteur_Message}! Vous avez atteint le niveau ${newLevel}! 🎉`
                 });
             }
-
             user.level = newLevel;
             await user.save();
         }
